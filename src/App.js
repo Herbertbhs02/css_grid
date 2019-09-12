bdb6095a5e52b15f5384d122a6211a84aec56759
Herbertbhs02 <33880154+Herbertbhs02@users.noreply.github.com>
2019-09-12 13:35:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const  App =()=> {
+    return (
+        <div className='container'>
+           <div className='item item1'>
+                <div className='pos1'>child1</div>
+                <div className='pos1'>child2</div>
+                <div className='pos1'>child3</div>
+                <div className='pos1'>child4</div>
+           </div>
+
+            <div className='item item2'>pos2</div>
+            <div className='item item3'>pos3</div>
+            <div className='item item4'>pos4</div>
+            <div className='item item5'>pos5</div>
+            <div className='item item6'>pos6</div>
+            <div className='item item7'>pos7</div>
+            <div className='item item8'>pos8</div>
+        </div>
+    );
+}
+
+export default App
